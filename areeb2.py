39d05e8e8fe3f553fc02f365f3455b2fb37b8dab
areebahmed17 <areebahmed17082008@gmail.com>
2026-03-19 13:40:34 +0500

    Delete areeb2.py

--- areeb2.py
+++
@@ -1,8 +0,0 @@
-#this is a commented line. 
-name = "Beau" ; print(name)
-number = "39"
-age = int(number) ; print(age)
-print(isinstance(name, str) and isinstance(age, int))
-name= "Beau"
-print(name.lower())
-print(name)
